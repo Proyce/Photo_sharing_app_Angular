d5c3c53a8c32c3cd947e5e9ffcea40960cb25d4b
Prosper Nnadi <prospernnadi3@gmail.com>
2023-04-10 20:02:09 +0100

    clean-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StartComponent } from './start/start.component';
 
 
 @NgModule({
   declarations: [
-
-  ],
+  StartComponent],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [StartComponent]
 })
 export class AppModule { }
