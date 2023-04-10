355c98e6a206409a97b5fa602b5b66aa7799e43b
Prosper Nnadi <prospernnadi3@gmail.com>
2023-04-10 22:53:11 +0100

    Added Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StartComponent } from './start/start.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { NewAccountComponent } from './pages/new-account/new-account.component';
 
+const Routes = [
+  { path: '', component: StartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'new-account', component: NewAccountComponent },
+  { path: '*', component: StartComponent },
+];
 
 @NgModule({
   declarations: [
-  StartComponent],
+    StartComponent,
+    NavBarComponent,
+    LoginComponent,
+    NewAccountComponent,
+  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(Routes),
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [StartComponent]
+  bootstrap: [StartComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/new-account/new-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+export class NewAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
